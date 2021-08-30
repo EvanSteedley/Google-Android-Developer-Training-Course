3858f23de499b99f62358ce4e2a11418c48475a4
EvanSteedley <43506468+EvanSteedley@users.noreply.github.com>
2021-08-29 21:24:55 -0400

    [Affirmations] Images added to each Affirmation
    Each Affirmation is now paired with an image that is displayed on top of it.

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -15,6 +16,7 @@ class ItemAdapter(private val context: Context,
     //Nested class
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     //Create new views, invoked by the layout manager
@@ -29,6 +31,7 @@ class ItemAdapter(private val context: Context,
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceID)
+        holder.imageView.setImageResource(item.imageResourceID)
     }
 
     //Returns the size of the dataset
